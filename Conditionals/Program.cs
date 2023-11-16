32cdc4f1a915a98469a83e61c7d06a7cd5ffe168
zeyne <zeyne@ZEYNEPDUZEN>
2023-11-16 16:28:02 +0300

    Add project files.

---
+++ Conditionals/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 30;
+
+            /* if (number == 30)
+             {
+                 Console.WriteLine("Number is 30");
+             }
+             else if (number == 20)
+             {
+                 Console.WriteLine("Number is 20");
+             }
+             else
+             {
+                 Console.WriteLine("Number is not 30 or 20");
+             } */
+
+
+
+            //single line if
+
+            /*var number2 = 22;
+
+            Console.WriteLine(number2 == 11 ? "Number2 is 11" : "Number2 is not 11");
+
+            var number3 = 33;
+            Console.WriteLine(number3 == 33 ? "Number3 is 33" : "Number3 is not 33");
+
+            var number4 = 55;
+            Console.WriteLine(number4 == 78 ? "Number4 is 78" : "Number4 is not 78");
+
+            var number5 = 11;
+            Console.WriteLine(number5 == 11 ? "Number5 is 11" : "Number5 is not 11");*/
+
+            //switch case
+
+            /*
+            switch (number)
+            {
+                case 10:
+                    Console.WriteLine("Number is 10");
+                    break;
+                case 20:
+                    Console.WriteLine("number is 20");
+                    break;
+                case 30:
+                    Console.WriteLine("Number is 30");
+                    break;
+                case 40:
+                    Console.WriteLine("Number is 40");
+                    break;
+                case 50:
+                    Console.WriteLine("Number is 50");
+                    break;
+                default:
+                    Console.WriteLine("Number is incorrect");
+                    break;
+            }
+            */
+
+            //multiple conditions
+
+            /*if (number >= 0 && number <= 100)
+            {
+                Console.WriteLine("Number is between 0 - 100");
+            }
+            else if (number > 100 && number <= 200)
+            {
+                Console.WriteLine("Number is between 101 - 200");
+            }
+            else if (number > 200 || number < 0)
+            {
+                Console.WriteLine("Number is less than 0 or greater than 200");
+            }*/
+
+            //please find the biggest number from three numbers that the user entered
+
+            int num1, num2, num3;
+
+            Console.WriteLine("Please enter the first number :");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("please enter the second number :");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("please enter the third number");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 < num2)
+            {
+                if (num1 < num3)
+                {
+                    Console.WriteLine("the smallest number is number 1");
+                }
+                else
+                {
+                    Console.WriteLine("the smallest number is number 3");
+                }
+            }
+            else if (num2 < num3)
+            {
+                Console.WriteLine("the smallest number is number 2");
+            }
+            else
+            {
+                Console.WriteLine("the smallest number is number 3");
+            }
+            
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TypesAndVariables/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesAndVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello world");
+
+            int number1 = 10;
+            //Console.WriteLine("Number1 is {0}",number1);
+            Console.WriteLine("Number1 is : " + number1);
+
+            int minIntDegeri = int.MinValue;
+            int maxIntDegeri = int.MaxValue;
+            Console.WriteLine("int min deger : " + int.MinValue);
+            Console.WriteLine("int max deger : " + int.MaxValue);
+
+            long minValue = long.MinValue;
+            long maxValue = long.MaxValue;
+            Console.WriteLine("long min value : " + long.MinValue);
+            Console.WriteLine("long max value : " + long.MaxValue);
+
+            short minShortDegeri = short.MinValue;
+            short maxShortDegeri = short.MaxValue;
+            Console.WriteLine("short min deger : " + short.MinValue);
+            Console.WriteLine("short max deger : " + short.MaxValue);
+
+            byte minByteDegeri = byte.MinValue;
+            byte maxByteDegeri = byte.MaxValue;
+            Console.WriteLine("byte min deger : " + byte.MinValue);
+            Console.WriteLine("byte max deger  : " + byte.MaxValue);
+
+            char karakter = 'b';
+            //Console.WriteLine("karakter : {0}", karakter);
+            Console.WriteLine("karakter : " +karakter);
+
+            //char veri tipine assign ettigimiz degeri int veri tipine donusturebiliriz:
+            Console.WriteLine("karakterin sayisal karsiligi : " + (int)karakter);
+
+            double doubleSayi2 = 25.4;
+            Console.WriteLine("double ondalikli sayi ornegi : " + doubleSayi2);
+
+            Console.WriteLine(Days.Wednesday);
+            Console.WriteLine((int)Days.Wednesday);
+
+            var num1 = 10;
+            Console.WriteLine("num1 : " + num1); //var degiskeni bir int deger tutuyor
+            var isim = "zeynep";
+            Console.WriteLine("isim : " + isim); //var degiskeni bir string deger tutuyor
+                 
+
+            Console.ReadLine();
+        }
+    }
+
+    enum Days
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
